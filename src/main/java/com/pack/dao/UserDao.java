f43f90821f00e5a50ea68b668ecde6d87f8dbe8d
bmdhaker <bmdhaker@gmail.com>
2023-04-25 23:03:47 +0100

    ajout de user repository

---
+++ src/main/java/com/pack/dao/UserDao.java
@@ -0,0 +1,14 @@
+package com.pack.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pack.model.DAOUser;
+
+
+@Repository
+public interface UserDao extends CrudRepository<DAOUser, Integer> {
+	
+	DAOUser findByUsername(String username);
+	
+}
