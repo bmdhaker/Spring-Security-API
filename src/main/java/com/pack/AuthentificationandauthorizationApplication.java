5d072db6c44f22b6a18d76e384d5c2143e220907
bmdhaker <bmdhaker@gmail.com>
2023-04-25 22:01:43 +0100

    creation projet spring boot vide

---
+++ src/main/java/com/pack/AuthentificationandauthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthentificationandauthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthentificationandauthorizationApplication.class, args);
+	}
+
+}
