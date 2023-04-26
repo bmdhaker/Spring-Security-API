5a73ea4fcd06ba86fa38907e660f51c5065845d1
bmdhaker <bmdhaker@gmail.com>
2023-04-26 12:52:46 +0100

    affichage du profil de l'utilisateur (username, email, password)

--- src/main/java/com/pack/controller/JwtAuthenticationController.java
+++ src/main/java/com/pack/controller/JwtAuthenticationController.java
@@ -11,12 +11,14 @@ import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pack.config.JwtTokenUtil;
+import com.pack.model.DAOUser;
 import com.pack.model.JwtRequest;
 import com.pack.model.JwtResponse;
 import com.pack.model.UserDTO;
@@ -55,6 +57,23 @@ public class JwtAuthenticationController {
 		return ResponseEntity.ok(userDetailsService.save(user));
 	}
 
+	@RequestMapping(method = RequestMethod.POST, value = "/profile")
+	public ResponseEntity<?> afficherProfile(@RequestBody JwtRequest authenticationRequest) throws Exception {
+		authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+		final UserDetails userDetails = userDetailsService
+			.loadUserByUsername(authenticationRequest.getUsername());
+		DAOUser user=new DAOUser();
+		user.setUsername(userDetails.getUsername());
+		user.setEmail(userDetails.getUsername());
+		user.setPassword(authenticationRequest.getPassword());
+		System.out.println("je suis dans profile");
+		//return userDetails;
+		return ResponseEntity.ok(user.toString());
+	}
+
+	
+	
+	
 	private void authenticate(String username, String password) throws Exception {
 		try {
 			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
@@ -64,4 +83,7 @@ public class JwtAuthenticationController {
 			throw new Exception("INVALID_CREDENTIALS", e);
 		}
 	}
+
+	
+
 }
--- src/main/java/com/pack/model/UserDTO.java
+++ src/main/java/com/pack/model/UserDTO.java
@@ -31,4 +31,6 @@ public class UserDTO {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+	
 }
